dcdced64969df22a8ac0648f10a60da82ee42426
Salam <SalamPopolama@yandex.ru>
2024-08-07 02:38:01 +0300

    based auth system

--- server/models/models.js
+++ server/models/models.js
@@ -1,7 +1,7 @@
 const sequelize = require('../db.js')
 const {DataTypes} = require('sequelize')
 
-const user = sequelize.define('user', {
+const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     email: {type: DataTypes.STRING, unique: true},
     username: {type: DataTypes.STRING, unique: true},
@@ -9,24 +9,24 @@ const user = sequelize.define('user', {
     role: {type: DataTypes.STRING, defaultValue: "USER"},
 })
 
-const order = sequelize.define('order', {
+const Order = sequelize.define('order', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     text: {type: DataTypes.STRING},
 })
 
-const executing_order = sequelize.define('executing_order', {
+const ExecutingOrder = sequelize.define('executing_order', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 })
 
-user.hasMany(order)
-order.belongsTo(user)
+User.hasMany(Order)
+Order.belongsTo(User)
 
-user.hasMany(executing_order)
-executing_order.belongsTo(user)
+User.hasMany(ExecutingOrder)
+ExecutingOrder.belongsTo(User)
 
-order.hasOne(executing_order)
-executing_order.belongsTo(order)
+Order.hasOne(ExecutingOrder)
+ExecutingOrder.belongsTo(Order)
 
 module.exports = {
-    user, order, executing_order
+    User, Order, ExecutingOrder
 }
