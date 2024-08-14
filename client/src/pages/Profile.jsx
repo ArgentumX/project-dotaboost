1688b7f2986c71164b0b9e6f6de97cb85e26b82e
Denis Chupritskiy <denischupritsky@gmail.com>
2024-08-14 23:44:59 +0300

    add proto profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,11 +14,23 @@ const Profile = observer(() => {
         navigate(MAINPAGE_ROUTE)
     }
 
-    return (
-        <div className="center">
-          <button onClick={() => logOut()}>Выйти</button> 
+    const uploadAvatar = () => {
+        alert()
+        return false
+    }
+
+    return (<>
+        <div className="profile">
+            <img className="profilePageImg" src="src/assets/img/axe.png" alt=""/>
+            <div className="profilePageInfo">
+                <img className="profilePageAvatar" src="src/assets/img/default_profile_icon.png" alt=""/>
+                <img className="profilePageEditIcon" role="button" onClick={uploadAvatar} src="src/assets/img/icon_edit.png"/>
+                <h1>{user.name}</h1>
+                <h4>Баланс: {user.balance.toFixed(2)} ₽</h4>
+                <button onClick={() => logOut()}>Выйти</button> 
+            </div>
         </div>
-    );
+    </>);
 });
 
 export default Profile;
