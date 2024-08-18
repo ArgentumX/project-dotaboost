95d8149cb5544717af009d07f93060539e595491
Salam <SalamPopolama@yandex.ru>
2024-08-18 21:00:10 +0300

    balance fix + long access token lifetime

--- server/dtos/user-dto.js
+++ server/dtos/user-dto.js
@@ -1,16 +1,17 @@
 module.exports = class UserDto {
-  id;
-  email;
-  username;
-  isActivated;
-  balance;
-  avatar;
+    id;
+    email;
+    username;
+    isActivated;
+    balance;
+    avatar;
 
-  constructor(model) {
-    this.id = model.id;
-    this.email = model.email;
-    this.isActivated = model.isActivated;
-    this.avatar = model.avatar;
-    this.username = model.username;
-  }
+    constructor(model) {
+        this.id = model.id;
+        this.email = model.email;
+        this.isActivated = model.isActivated;
+        this.avatar = model.avatar;
+        this.balance = model.balance;
+        this.username = model.username;
+    }
 };
