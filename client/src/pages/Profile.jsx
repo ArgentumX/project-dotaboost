ee3dbcc385977564737377ef72a5a8a06fe65dbd
Denis Chupritskiy <denischupritsky@gmail.com>
2024-08-19 18:29:01 +0300

    made an INSANE image upload ui (not functional yet tho)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,24 +1,20 @@
 import React, { useContext, useState } from "react";
 import { Context } from "..";
 import { observer } from "mobx-react-lite";
-import ImageUpload from "../components/ImageUpload";
+import { toggleImageUpload } from "../components/ImageUpload";
 
 const Profile = observer(() => {
     const {store} = useContext(Context)
 
-    const [showImageUpload, setShowImageUpload] = useState(false);
-    const uploadAvatar = () => {
-        setShowImageUpload(true)
-        return false
-    }
-
     return (<>
         <div className="profile">
             <img className="profilePageImg" src="src/assets/img/axe.png" alt=""/>
-            { showImageUpload ? <ImageUpload/> : null }
             <div className="profilePageInfo">
                 <img className="profilePageAvatar" src="src/assets/img/default_profile_icon.png" alt=""/>
-                <img className="profilePageEditIcon" role="button" onClick={uploadAvatar} src="src/assets/img/icon_edit.png"/>
+                <img className="profilePageEditIcon" role="button" onClick={() => {
+                    toggleImageUpload();
+                    return false;
+                }} src="src/assets/img/icon_edit.png"/>
                 <h1>{store.user.username}</h1>
                 <h4>Баланс: {store.user.balance.toFixed(2)} ₽</h4>
                 <button onClick={ () => store.logout() }>Выйти</button> 
