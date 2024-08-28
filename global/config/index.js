22f0b47ca43b412f3e2d6322ad89fc9fa6b4ad53
Salam <SalamPopolama@yandex.ru>
2024-08-28 22:57:20 +0300

    feat: replaced integral price calculator with interval price calculator

--- global/config/index.js
+++ global/config/index.js
@@ -5,8 +5,11 @@ module.exports = {
             PRIORITY_MULTIPLIER: 1.3,
             STEAM_GUARD_MULTIPLIER: 1.0,
             PLAY_TIME_MULTIPLIER: 1.2,
-            // ax^n + bx^(n-1) + ... + cx + d
-            INTEGRAL_COEFFICIENTS: [0.000000000011, -0.000000063, 0.00018, 0.94, 0],
+            RATING_PRICE_MAP: {
+                "0-100": 1,
+                "100-150": 10,
+                "150-9999": 15,
+            },
         },
         PLAY_TIME_KEYS: ["MORNING", "NIGHT", "AFTERNOON", "EVENING"],
     },
--- global/prices/price-calculator.js
+++ global/prices/price-calculator.js
@@ -1,17 +1,5 @@
 const config = require("../config");
 
-function integrate(x1, x2, coefficients) {
-    return f(x2, coefficients) - f(x1, coefficients);
-}
-
-function f(x, coefficients) {
-    let result = 0;
-    for (let i = 0; i < coefficients.length; i++) {
-        result += coefficients[i] * x ** (coefficients.length - 1 - i);
-    }
-    return result;
-}
-
 function isSpecificPlayTime(playTime) {
     for (const key of config.ORDER.PLAY_TIME_KEYS) {
         if (!playTime[key]) {
@@ -20,13 +8,64 @@ function isSpecificPlayTime(playTime) {
     }
     return false;
 }
+class RatingPriceMap {
+    ratingPriceList;
+    constructor(rawPriceMap) {
+        this.ratingPriceList = [];
+
+        for (const key of Object.keys(rawPriceMap)) {
+            const splitted = key.split("-");
+            const a = splitted[0];
+            const b = splitted[1];
+            const element = { start: a, end: b, price: rawPriceMap[key] };
+            this.ratingPriceList.push(element);
+        }
+    }
+
+    getTotalPrice(startRating, endRating) {
+        let result = 0;
+        let startSector;
+        let endSector;
+        for (let i = 0; i < this.ratingPriceList.length; i++) {
+            if (startRating <= this.ratingPriceList[i].end) {
+                startSector = i;
+                break;
+            }
+        }
+        for (let i = startSector; i < this.ratingPriceList.length; i++) {
+            if (endRating <= this.ratingPriceList[i].end) {
+                endSector = i;
+                break;
+            }
+        }
+
+        if (startSector === endSector) {
+            result = (endRating - startRating) * this.ratingPriceList[startSector].price;
+        } else {
+            result +=
+                (this.ratingPriceList[startSector].end - startRating) *
+                this.ratingPriceList[startSector].price;
+            result +=
+                (endRating - this.ratingPriceList[endSector].start) *
+                this.ratingPriceList[endSector].price;
+            for (let i = startSector + 1; i < endSector; i++) {
+                result +=
+                    (this.ratingPriceList[i].end - this.ratingPriceList[i].start) *
+                    this.ratingPriceList[i].price;
+            }
+        }
+        return result;
+    }
+}
 class PriceCalculator {
+    ratingPriceMap = new RatingPriceMap(config.ORDER.PRICES.RATING_PRICE_MAP);
+
     // Send only validated data and use try/catch protection.
     getOrderPrice(order) {
         const { party, priority, steamGuard, playTime, startRating, endRating } = order;
         let price = 0;
 
-        price += integrate(startRating, endRating, config.ORDER.PRICES.INTEGRAL_COEFFICIENTS);
+        price += this.ratingPriceMap.getTotalPrice(startRating, endRating);
         price *= party ? config.ORDER.PRICES.PARTY_MULTIPLIER : 1.0;
         price *= priority ? config.ORDER.PRICES.PRIORITY_MULTIPLIER : 1.0;
         price *= steamGuard ? config.ORDER.PRICES.STEAM_GUARD_MULTIPLIER : 1.0;
