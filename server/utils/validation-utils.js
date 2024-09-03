d7dbad0bbbbf278051d17cd1939304975c96b1d2
Salam <SalamPopolama@yandex.ru>
2024-09-03 21:32:05 +0300

    fix: fixed export crush

--- server/utils/validation-utils.js
+++ server/utils/validation-utils.js
@@ -1,6 +1,7 @@
-export function isNonNegative(num) {
+function isNonNegative(num) {
     if (num < 0) {
         throw new Error("offset must be positive");
     }
     return true;
 }
+module.exports = { isNonNegative };
