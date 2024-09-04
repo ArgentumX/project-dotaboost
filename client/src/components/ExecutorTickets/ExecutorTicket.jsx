7ca77dd5e2596d462e85841a5e22f218e0bf6a4a
Salam <SalamPopolama@yandex.ru>
2024-09-04 21:58:43 +0300

    fix: fixed avatar image wrong path

--- client/src/components/ExecutorTickets/ExecutorTicket.jsx
+++ client/src/components/ExecutorTickets/ExecutorTicket.jsx
@@ -10,56 +10,76 @@ const ExecutorTicket = ({ ticket }) => {
         const newShowImage = [...showImage];
         newShowImage[id] = !newShowImage[id];
         setShowImage(newShowImage);
-    }
+    };
 
     const handleApplyClick = (id) => {
         AdminService.verify(id, true);
-        document.getElementById(id.toString()).classList.toggle(styles['ticket-closed']);
-
-    }
+        document.getElementById(id.toString()).classList.toggle(styles["ticket-closed"]);
+    };
 
     const handleRejectClick = (id) => {
         AdminService.verify(id, false);
-        document.getElementById(id.toString()).classList.toggle(styles['ticket-closed']);
-    }
+        document.getElementById(id.toString()).classList.toggle(styles["ticket-closed"]);
+    };
 
     return (
-
         <div className={styles["ticket"]} id={ticket.id}>
-            <h4><b>ID: </b>{ticket.id}</h4>
-            {ticket.verified ?
-                <h4 className="green"><b>Verified: </b>true</h4>
-                :
-                <h4 className="red"><b>Verified: </b>false</h4>
-            }
-            {ticket.paid ?
-                <h4 className="green"><b>Paid: </b>true</h4>
-                :
-                <h4 className="red"><b>Paid: </b>false</h4>
-            }
-            {ticket.closed ?
-                <h4 className="green"><b>Closed: </b>true</h4>
-                :
-                <h4 className="red"><b>Closed: </b>false</h4>
-            }
-            <h4><b>UserID: </b>{ticket.userId}</h4>
-            <h4><b>Required Username: </b>{ticket.requiredUsername}</h4>
-            <h4
-                onClick={() => handleImageClick(ticket.id)}
-                className="nonselectable"
-            >
+            <h4>
+                <b>ID: </b>
+                {ticket.id}
+            </h4>
+            {ticket.verified ? (
+                <h4 className="green">
+                    <b>Verified: </b>true
+                </h4>
+            ) : (
+                <h4 className="red">
+                    <b>Verified: </b>false
+                </h4>
+            )}
+            {ticket.paid ? (
+                <h4 className="green">
+                    <b>Paid: </b>true
+                </h4>
+            ) : (
+                <h4 className="red">
+                    <b>Paid: </b>false
+                </h4>
+            )}
+            {ticket.closed ? (
+                <h4 className="green">
+                    <b>Closed: </b>true
+                </h4>
+            ) : (
+                <h4 className="red">
+                    <b>Closed: </b>false
+                </h4>
+            )}
+            <h4>
+                <b>UserID: </b>
+                {ticket.userId}
+            </h4>
+            <h4>
+                <b>Required Username: </b>
+                {ticket.requiredUsername}
+            </h4>
+            <h4 onClick={() => handleImageClick(ticket.id)} className="nonselectable">
                 <b>{showImage[ticket.id] ? "▾" : "▸"} Image:</b>
             </h4>
-            {showImage[ticket.id] && <img src={API_URL + ticket.image} />}
+            {showImage[ticket.id] && <img src={API_URL + "images/" + ticket.image} />}
             {!ticket.closed && !ticket.verified && (
-
                 <div className={styles["ticket-footer"]}>
                     <button onClick={() => handleApplyClick(ticket.id)}>Apply</button>
-                    <button onClick={() => handleRejectClick(ticket.id)} className={styles["second-button"]}>Reject</button>
+                    <button
+                        onClick={() => handleRejectClick(ticket.id)}
+                        className={styles["second-button"]}
+                    >
+                        Reject
+                    </button>
                 </div>
             )}
         </div>
     );
-}
+};
 
 export default ExecutorTicket;
--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -1,24 +1,23 @@
-import { makeAutoObservable } from "mobx"
-import AuthService from "../service/AuthService"
-import axios from "axios"
-import { API_URL } from "../http/axios"
+import { makeAutoObservable } from "mobx";
+import AuthService from "../service/AuthService";
+import axios from "axios";
+import { API_URL } from "../http/axios";
 import UserService from "../service/UserService";
 
 export default class UserStore {
-
     constructor() {
         this.user = {};
         this.isAuth = false;
         this.isLoading = false;
-        makeAutoObservable(this)
+        makeAutoObservable(this);
     }
 
     setAuth(bool) {
-        this.isAuth = bool
+        this.isAuth = bool;
     }
 
     setUser(user) {
-        this.user = user
+        this.user = user;
     }
 
     setLoading(bool) {
@@ -28,105 +27,104 @@ export default class UserStore {
     async login(email, password) {
         try {
             const response = await AuthService.login(email, password);
-            localStorage.setItem('token', response.data.accessToken);
+            localStorage.setItem("token", response.data.accessToken);
             this.setAuth(true);
             this.setUser(response.data.user);
-            this.user.avatar = this.user.avatar ? API_URL + this.user.avatar : null;
+            this.user.avatar = this.user.avatar ? API_URL + "images/" + this.user.avatar : null;
         } catch (e) {
-            if (e.response?.data?.message == 'Wrong email or password') {
+            if (e.response?.data?.message == "Wrong email or password") {
                 swal({
                     title: "Oшибка",
                     text: "Неверный пароль или почта.",
-                    icon: "error"
-                })
+                    icon: "error",
+                });
 
                 return;
             }
             swal({
                 title: "Ошибка",
                 text: e.response?.data?.message,
-                icon: "error"
-            })
+                icon: "error",
+            });
         }
     }
 
     async registration(email, username, password) {
         try {
             const response = await AuthService.registration(email, username, password);
-            localStorage.setItem('token', response.data.accessToken);
+            localStorage.setItem("token", response.data.accessToken);
             this.setAuth(true);
             this.setUser(response.data.user);
         } catch (e) {
-            if (e.response?.data?.message == 'email is already in use') {
+            if (e.response?.data?.message == "email is already in use") {
                 swal({
                     title: "Oшибка",
                     text: "Почта уже используется.",
-                    icon: "error"
-                })
+                    icon: "error",
+                });
 
                 return;
             }
 
-            if (e.response?.data?.message == 'username is already in use') {
+            if (e.response?.data?.message == "username is already in use") {
                 swal({
                     title: "Oшибка",
                     text: "Данное имя пользователя уже используется.",
-                    icon: "error"
-                })
+                    icon: "error",
+                });
 
                 return;
             }
 
-            if (e.response?.data?.message == 'validation error') {
-                if (e.response.data.errors[0].path == 'username') {
+            if (e.response?.data?.message == "validation error") {
+                if (e.response.data.errors[0].path == "username") {
                     swal({
                         title: "Ошибка",
                         text: "Имя пользователя слишком длинное или слишком короткое.",
-                        icon: "error"
-                    })
+                        icon: "error",
+                    });
                     return;
                 }
 
-                if (e.response.data.errors[0].path == 'email') {
+                if (e.response.data.errors[0].path == "email") {
                     swal({
                         title: "Ошибка",
                         text: "Неверная почта.",
-                        icon: "error"
-                    })
+                        icon: "error",
+                    });
                     return;
                 }
 
-                if (e.response.data.errors[0].path == 'password') {
+                if (e.response.data.errors[0].path == "password") {
                     swal({
                         title: "Ошибка",
                         text: "Слишком слабый пароль.",
-                        icon: "error"
-                    })
+                        icon: "error",
+                    });
                     return;
                 }
-
             }
 
             swal({
                 title: "Ошибка",
                 text: e.response?.data?.message,
-                icon: "error"
-            })
+                icon: "error",
+            });
         }
     }
 
     async logout() {
         try {
             const response = await AuthService.logout();
-            localStorage.removeItem('token');
+            localStorage.removeItem("token");
             this.setAuth(false);
             this.setUser({});
         } catch (e) {
             swal({
                 title: "Ошибка",
                 text: e.response?.data?.message,
-                icon: "error"
-            })
+                icon: "error",
+            });
         }
     }
 
@@ -134,14 +132,16 @@ export default class UserStore {
         this.setLoading(true);
 
         try {
-            const response = await axios.get(`${API_URL}api/user/refresh`, { withCredentials: true })
+            const response = await axios.get(`${API_URL}api/user/refresh`, {
+                withCredentials: true,
+            });
 
-            response.data.accessToken ?
-                localStorage.setItem('token', response.data.accessToken) :
-                localStorage.removeItem('token')
+            response.data.accessToken
+                ? localStorage.setItem("token", response.data.accessToken)
+                : localStorage.removeItem("token");
             this.setAuth(true);
             this.setUser(response.data.user);
-            this.user.avatar = this.user.avatar ? API_URL + this.user.avatar : null;
+            this.user.avatar = this.user.avatar ? API_URL + "images/" + this.user.avatar : null;
         } catch (e) {
         } finally {
             this.setLoading(false);
@@ -151,13 +151,13 @@ export default class UserStore {
     async uploadAvatar(avatar) {
         try {
             const response = await UserService.uploadAvatar(avatar);
-            this.user.avatar = API_URL + response.data.avatar;
+            this.user.avatar = API_URL + "images/" + response.data.avatar;
         } catch (e) {
             swal({
                 title: "Ошибка",
                 text: e.response?.data?.message,
-                icon: "error"
-            })
+                icon: "error",
+            });
         }
     }
 }
